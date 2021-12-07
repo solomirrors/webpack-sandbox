99301f2c410b1cfe19a1ba717ec5b7aa2bee5ee2
solomirrors <solomirrors@icloud.com>
2021-12-08 00:41:08 +0100

    React и Babel Loader

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
-import Log from "./log";
-import Calc from "./calc";
-import logo from './React-logo.png'
+import React from "react";
+import ReactDOM from "react-dom";
 
+const App = () => <p>Hello, Webpack!</p>
 
-const calc = new Calc();
-const log = new Log();
-
-log.log(calc.add(1, 2, 3, 4))
-
-const el = document.createElement('img');
-el.src = logo;
-document.body.appendChild(el)
+ReactDOM.render(<App/>, document.getElementById('root'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,41 @@
 module.exports = {
     mode: "development",
     module: {
+        // Loading Images
         rules: [
             {
-                test: /\.png$/,
-                use: [{loader: "file-loader"}]
+              test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "babel-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|ico)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            outputPath: 'images',
+                            name: '[name]-[sha1:hash:7].[ext]'
+                        }
+                    }
+                ]
+            },
+            //Loading Fonts
+            {
+                test: /\.(ttf|otf|eot|woff|woff2)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            outputPath: 'images',
+                            name: '[name].[ext]'
+                        }
+                    }
+                ]
             }
         ]
     }
